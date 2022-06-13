920df1b3da2921954221b030d1b611148435b0cf
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-06-13 12:59:47 +0530

    [shravanthi] - Create Database and connect to DB

---
+++ AddressBookDB_ADO.net/AdressBookRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDB_ADO.net
+{
+    public class AddressBookRepo
+    {
+        public void Create_Database()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=LAPTOP-2UH1FDRP\MSSQLSERVER01; Initial Catalog =master; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand cmd = new SqlCommand("Create database AddressBookForADO;", Connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("AddressbookService Database created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating database:" + e.Message + "\t");
+            }
+        }
+    }
+}
--- AddressBookDB_ADO.net/Program.cs
+++ AddressBookDB_ADO.net/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBookDB_ADO.net;
+
 Console.WriteLine("Hello, AddressBook ADO.net!");
+AddressBookRepo address = new();
+address.Create_Database();
